075c7989cacd4a6ede5cf2b919dbefef6987a493
Marton Somogyi <mcbuddha@gmail.com>
2012-10-04 00:59:07 +0200

    webview settings

--- android_app/src/su/eket/festapp/EntryPointActivity.java
+++ android_app/src/su/eket/festapp/EntryPointActivity.java
@@ -1,15 +1,25 @@
 package su.eket.festapp;
 
+import android.webkit.WebSettings;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
-public class EntryPointActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+public class EntryPointActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+	
+	WebView webview = ((WebView) findViewById(R.id.webview));
+	WebSettings websettings = webview.getSettings();
+	websettings.setJavaScriptEnabled(true);
+	websettings.setGeolocationEnabled(false);
+	websettings.setUseWideViewPort(true);
+	websettings.setSupportZoom(false);
+	websettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+
+	webview.loadUrl(getString(R.string.webapp_url));
+	return;
     }
 }
